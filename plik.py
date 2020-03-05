942a9aa06b9fda13bc890df269484af6a5a3109a
michalj11121 <michalj11121@wp.pl>
2020-03-05 10:55:32 +0100

    jeden

---
+++ plik.py
@@ -0,0 +1,127 @@
+#ZAD 1
+
+#x = input()
+#print(x.count(" "))
+
+#ZAD 2
+
+#import sys
+#x = int(sys.stdin.readline())
+#y = int(sys.stdin.readline())
+
+#z = str(x*y)
+#sys.stdout.write(z)
+
+#ZAD 3
+
+# < =< > >= != == OR AND
+
+#ZAD 4
+
+#x=int(input())
+
+#if x<0:
+   # print(x*-1)
+#else:
+  #  print(x)
+
+  #ZAD 5
+
+#a=int(input())
+#b=int(input())
+#c=int(input())
+#if a>0 and a<=10 and a>b or b>c:
+#    print("spełnia wszystkie warunki")
+#else:
+    #print("nie spełnia warunków")
+
+#ZAD 6 
+
+#x=5
+#while x<100:
+  #print(x)
+  #x=x+5
+
+#ZAD 7
+
+
+
+#while True:
+  #print("Podaj liczbe")
+  #x=int(input())
+  #print("kwadrat tej liczby to : ",x*x)
+    
+#ZAD 8
+
+#lista=[]
+#while True:
+ #print("Podaj liczbe")
+ #x=int(input())
+ #lista.append(x)
+ #print(lista)
+
+ #ZAD 9
+
+#x = input()
+#y = str(x)
+#i=0
+#lista=[]
+#while i<len(y):
+#  lista.append(y[i])
+#  i=i+1
+#print(lista)
+#lista=list(map(int,lista))
+#print(sum(lista))
+
+#ZAD 10
+#print('Podaj liczbe nie wieksza od 10 ')
+#x = int(input())
+#i=0
+#if  x>10:
+#  print("blad")
+#else:
+#  while i<=x:
+#    print(i*"A")
+#    i=i+1
+
+#ZAD 11
+
+#x=5
+#y=1
+#n=int(input("Podaj liczbe"))
+#while y<n:
+#  print(x*" ",y*"o")
+#  x=x-1
+#  y=y+2
+#print (x*" ",y*"o")
+#while x<n+2:
+#  x=x+1
+#  y=y-2
+#  print(x*" ",y*"o")
+
+#ZAD 12
+
+#for x in range (1,10+1):
+ # for y in range (1,10+1):
+  #  print(x,"*",y," = ",x*y)
+
+#ZAD 13
+
+#ZAD 14
+
+#x=int(input("Podaj liczbe ktorej pierwiastek chcesz obliczyc"))
+#from math import *
+#try:
+#  wynik=sqrt(x)
+ # print(wynik)
+#except ValueError:
+#  print("nie mozna pierwiastkowac liczby ujemnej")
+
+#ZAD 15
+
+#while True:
+  #try:
+    #x = int(input("Podaj liczbe: "))
+    #break
+  #except ValueError:  print("To nie liczba")
+  
